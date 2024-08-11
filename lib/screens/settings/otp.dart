d6152771c00e2e65e5aa5ad982b264a4d45b0ce2
Genre Mamanao <41022228+Faun471@users.noreply.github.com>
2024-08-11 16:00:11 +0800

    Code cleanup

--- lib/screens/settings/otp.dart
+++ lib/screens/settings/otp.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Otp extends StatelessWidget {
- const Otp({ Key? key}) : super(key: key);
+  const Otp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,22 +16,22 @@ class Otp extends StatelessWidget {
             children: [
               GestureDetector(
                 onTap: () => Navigator.pop(context),
-                child: Icon(
+                child: const Icon(
                   Icons.arrow_back,
                   size: 32,
                   color: Colors.black54,
                 ),
               ),
-              SizedBox(height: 18),
-              Text(
+              const SizedBox(height: 18),
+              const Text(
                 'Verification',
                 style: TextStyle(
                   fontSize: 22,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 10),
-              Text(
+              const SizedBox(height: 10),
+              const Text(
                 "Enter your OTP code number",
                 style: TextStyle(
                   fontSize: 14,
@@ -40,7 +40,7 @@ class Otp extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 28),
+              const SizedBox(height: 28),
               Container(
                 padding: const EdgeInsets.all(28),
                 decoration: BoxDecoration(
@@ -58,19 +58,20 @@ class Otp extends StatelessWidget {
                         _textFieldOTP(context, first: false, last: true),
                       ],
                     ),
-                    SizedBox(height: 22),
+                    const SizedBox(height: 22),
                     SizedBox(
                       width: double.infinity,
                       child: ElevatedButton(
                         onPressed: () {},
                         style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white, backgroundColor: Colors.orange,
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.orange,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(24.0),
                           ),
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(14.0),
+                        child: const Padding(
+                          padding: EdgeInsets.all(14.0),
                           child: Text(
                             'Verify',
                             style: TextStyle(fontSize: 16),
@@ -81,8 +82,8 @@ class Otp extends StatelessWidget {
                   ],
                 ),
               ),
-              SizedBox(height: 18),
-              Text(
+              const SizedBox(height: 18),
+              const Text(
                 "Didn't you receive any code?",
                 style: TextStyle(
                   fontSize: 14,
@@ -91,8 +92,8 @@ class Otp extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 18),
-              Text(
+              const SizedBox(height: 18),
+              const Text(
                 "Resend New Code",
                 style: TextStyle(
                   fontSize: 18,
@@ -109,7 +110,7 @@ class Otp extends StatelessWidget {
   }
 
   Widget _textFieldOTP(BuildContext context, {required bool first, last}) {
-    return Container(
+    return SizedBox(
       height: 85,
       child: AspectRatio(
         aspectRatio: 1.0,
@@ -119,24 +120,24 @@ class Otp extends StatelessWidget {
             if (value.length == 1 && last == false) {
               FocusScope.of(context).nextFocus();
             }
-            if (value.length == 0 && first == false) {
+            if (value.isEmpty && first == false) {
               FocusScope.of(context).previousFocus();
             }
           },
           showCursor: false,
           readOnly: false,
           textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           keyboardType: TextInputType.number,
           maxLength: 1,
           decoration: InputDecoration(
-            counter: Offstage(),
+            counter: const Offstage(),
             enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(width: 2, color: Colors.black12),
+              borderSide: const BorderSide(width: 2, color: Colors.black12),
               borderRadius: BorderRadius.circular(12),
             ),
             focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(width: 2, color: Colors.purple),
+              borderSide: const BorderSide(width: 2, color: Colors.purple),
               borderRadius: BorderRadius.circular(12),
             ),
           ),
@@ -145,4 +146,3 @@ class Otp extends StatelessWidget {
     );
   }
 }
-
